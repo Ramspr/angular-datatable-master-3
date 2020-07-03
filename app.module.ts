c1a94f06eb104e527e332929079a0872336f018c
Ramspr <66228940+Ramspr@users.noreply.github.com>
2020-07-03 15:48:07 +0530

    test
    test

--- app.module.ts
+++ app.module.ts
@@ -1,29 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgxDatatableModule } from '@swimlane/ngx-datatable';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { MaterialModule } from './material.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import 'hammerjs';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    HttpClientModule,
     BrowserModule,
-    BrowserAnimationsModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    NgxDatatableModule,
-    FlexLayoutModule,
-    MaterialModule
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
